13035446e8fd9ee2e407a5a94af0abfe1a1fc9b6
emilyzfliu <emily.zf.liu@gmail.com>
2026-05-19 22:50:27 -0700

    fix bash

--- experiments.sh
+++ experiments.sh
@@ -1,15 +1,15 @@
-for i in {0..50..5}; do
+for i in $(seq 0 5 50); do
     python main.py --ptb-type 'char' --ptb-pct $i
 done
 
-for i in {5..50..5}; do
+for i in $(seq 5 5 50); do
     python main.py --ptb-type 'token' --ptb-pct $i
 done
 
-for i in {5..50..5}; do
+for i in $(seq 5 5 50); do
     python main.py --ptb-type 'shuffle' --ptb-pct $i
 done
 
-for i in {5..50..5}; do
+for i in $(seq 5 5 50); do
     python main.py --ptb-type 'noise' --ptb-pct $i
 done
