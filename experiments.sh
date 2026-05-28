f12f8a28df6a82e92ee67464ec7db9ea76eaa0ed
emilyzfliu <emily.zf.liu@gmail.com>
2026-05-27 21:09:44 -0700

    finishing touches

--- experiments.sh
+++ experiments.sh
@@ -1,15 +1,15 @@
 # Base experiments
-# for i in $(seq 0 5 50); do
-#     python main.py --ptb-type 'char' --ptb-pct $i
-# done
+for i in $(seq 0 5 50); do
+    python main.py --ptb-type 'char' --ptb-pct $i
+done
 
-# for i in $(seq 5 5 50); do
-#     python main.py --ptb-type 'token' --ptb-pct $i
-# done
+for i in $(seq 5 5 50); do
+    python main.py --ptb-type 'token' --ptb-pct $i
+done
 
-# for i in $(seq 0 5 100); do
-#     python main.py --ptb-type 'shuffle' --ptb-pct $i
-# done
+for i in $(seq 0 5 100); do
+    python main.py --ptb-type 'shuffle' --ptb-pct $i
+done
 
 # Ablation experiments - Only run after base
 python ablations.py --ptb-type 'char'
