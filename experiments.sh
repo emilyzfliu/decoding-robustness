c3e6ef80df994f74798d1d3b86769e6c267d925a
emilyzfliu <emily.zf.liu@gmail.com>
2026-07-11 23:42:05 -0700

    lump ptb pct into one run to minimize gpu setup overhead

--- experiments.sh
+++ experiments.sh
@@ -1,16 +1,7 @@
 # Base experiments
-for i in $(seq 0 5 50); do
-    python main.py --model 'gpt2' --ptb-type 'char' --ptb-pct $i
-done
-
-for i in $(seq 5 5 50); do
-    python main.py --model 'gpt2' --ptb-type 'token' --ptb-pct $i
-done
-
-for i in $(seq 5 5 100); do
-    python main.py --model 'gpt2' --ptb-type 'shuffle' --ptb-pct $i
-done
-
+python main.py --model 'gpt2' --ptb-type 'char'
+python main.py --model 'gpt2' --ptb-type 'token'
+python main.py --model 'gpt2' --ptb-type 'shuffle'
 # Ablation experiments - Only run after base
 # python ablations.py --ptb-type 'char'
 # python ablations.py --ptb-type 'token'
--- main.py
+++ main.py
@@ -13,73 +13,75 @@ from config import MODEL_INFO
 def run(args):
     SEQ_LEN = 128 if not args.debug else 5
     ptb_type = args.ptb_type
-    ptb_pct = args.ptb_pct
-    model_name = MODEL_INFO[args.model]['model_name']
-    
-    rng = random.Random(args.seed)
+    ptb_pcts = [args.ptb_pct] if args.ptb_pct != -1 else [x*5 for x in range(1, 11)] if ptb_type != 'shuffle' else [x*5 for x in range(1, 21)]
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Set up models
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, attn_implementation=MODEL_INFO[args.model]['attn_implementation']).to(device)
-
-    # Set up dataset
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "left"
-
-    ds = load_dataset("Salesforce/wikitext", "wikitext-2-raw-v1")
-
-    texts = list(ds['test']['text'])
-    texts = [x for x in texts if len(x.split()) > SEQ_LEN]
-
-    rng_data = random.Random(1)
-
-    # texts = rng_data.sample(texts, 100)
-
-    if args.debug:
-        texts = [
-            'Lorem ipsum dolor sit amet',
-            'Hello world! Hello universe?'
-        ]
-
-    texts_perturbed = perturb(texts, ptb_pct, rng, ptb_type, tokenizer)
-
-
-    BATCH_SIZE = 128 if torch.cuda.is_available() else 4
-
-    from tqdm import tqdm
-
-    os.makedirs(f'results_{model_name}/{ptb_type}/{ptb_pct}', exist_ok=True)
-
-    try:
-        seen = set(pd.read_csv(f'results_{model_name}/{ptb_type}/{ptb_pct}/evals.csv')['sample'])
-    except:
-        seen = set()
-
-    for i in tqdm(range(0, len(texts), BATCH_SIZE), desc=f"{ptb_type} pct={ptb_pct}"):
-        batch_texts = texts[i:i+BATCH_SIZE]
-        batch_texts_perturbed = texts_perturbed[i:i+BATCH_SIZE]
+    for ptb_pct in ptb_pcts:
+        model_name = MODEL_INFO[args.model]['model_name']
         
-        inputs = tokenizer(batch_texts, return_tensors="pt", 
-                        truncation=True, max_length=128, padding='max_length').to(device)
-        inputs_perturbed = tokenizer(batch_texts_perturbed, return_tensors="pt",
-                                    truncation=True, max_length=128, padding='max_length').to(device)
-        
-        with torch.no_grad():
-            # TODO set output_hidden_states back to True if we want to compute activation similarity
-            outputs = model(**inputs, output_hidden_states=False, output_attentions=True)
-            outputs_perturbed = model(**inputs_perturbed, output_hidden_states=False, output_attentions=True)
-        
-        res = eval_loop(inputs, outputs, inputs_perturbed, outputs_perturbed, tokenizer, i)
+        rng = random.Random(args.seed)
 
-        res = res[~res['sample'].isin(seen)]
-        if not args.debug:
-            res.to_csv(f'results_{model_name}/{ptb_type}/{ptb_pct}/evals.csv', 
-                                    mode='a', header=(i==0 and len(seen) == 0), index=False)
-        else:
-            res.to_csv(f'results_{model_name}/debug.csv', mode='a', header=(i==0 and len(seen) == 0), index=False)
-        
-        del outputs, outputs_perturbed
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Set up models
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name, attn_implementation=MODEL_INFO[args.model]['attn_implementation']).to(device)
+
+        # Set up dataset
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = "left"
+
+        ds = load_dataset("Salesforce/wikitext", "wikitext-2-raw-v1")
+
+        texts = list(ds['test']['text'])
+        texts = [x for x in texts if len(x.split()) > SEQ_LEN]
+
+        rng_data = random.Random(1)
+
+        # texts = rng_data.sample(texts, 100)
+
+        if args.debug:
+            texts = [
+                'Lorem ipsum dolor sit amet',
+                'Hello world! Hello universe?'
+            ]
+
+        texts_perturbed = perturb(texts, ptb_pct, rng, ptb_type, tokenizer)
+
+
+        BATCH_SIZE = 128 if torch.cuda.is_available() else 4
+
+        from tqdm import tqdm
+
+        os.makedirs(f'results_{model_name}/{ptb_type}/{ptb_pct}', exist_ok=True)
+
+        try:
+            seen = set(pd.read_csv(f'results_{model_name}/{ptb_type}/{ptb_pct}/evals.csv')['sample'])
+        except:
+            seen = set()
+
+        for i in tqdm(range(0, len(texts), BATCH_SIZE), desc=f"{ptb_type} pct={ptb_pct}"):
+            batch_texts = texts[i:i+BATCH_SIZE]
+            batch_texts_perturbed = texts_perturbed[i:i+BATCH_SIZE]
+            
+            inputs = tokenizer(batch_texts, return_tensors="pt", 
+                            truncation=True, max_length=128, padding='max_length').to(device)
+            inputs_perturbed = tokenizer(batch_texts_perturbed, return_tensors="pt",
+                                        truncation=True, max_length=128, padding='max_length').to(device)
+            
+            with torch.no_grad():
+                # TODO set output_hidden_states back to True if we want to compute activation similarity
+                outputs = model(**inputs, output_hidden_states=False, output_attentions=True)
+                outputs_perturbed = model(**inputs_perturbed, output_hidden_states=False, output_attentions=True)
+            
+            res = eval_loop(inputs, outputs, inputs_perturbed, outputs_perturbed, tokenizer, i)
+
+            res = res[~res['sample'].isin(seen)]
+            if not args.debug:
+                res.to_csv(f'results_{model_name}/{ptb_type}/{ptb_pct}/evals.csv', 
+                                        mode='a', header=(i==0 and len(seen) == 0), index=False)
+            else:
+                res.to_csv(f'results_{model_name}/debug.csv', mode='a', header=(i==0 and len(seen) == 0), index=False)
+            
+            del outputs, outputs_perturbed
 
 
 if __name__ == "__main__":
@@ -87,7 +89,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--model", help="Model name", type=str, default='gpt2')
     parser.add_argument("--ptb-type", help="Perturbation type: ['char', 'token', 'shuffle']", type=str, default='char')
-    parser.add_argument("--ptb-pct", help="Percent of input text perturbed", type=int, default=0)
+    parser.add_argument("--ptb-pct", help="Percent of input text perturbed", type=int, default=-1)
     parser.add_argument("--seed", help="Random seed", type=int, default=1)
     parser.add_argument("--debug", action='store_true')
 
