d2cf95c44bc62f8029f4cbbe974f6ffff4a4ff09
emilyzfliu <emily.zf.liu@gmail.com>
2026-07-25 00:16:54 -0700

    fix syntax

--- experiments.sh
+++ experiments.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 # python main.py --model 'gpt2' --ptb-type 'token'
 # python main.py --model 'gpt2' --ptb-type 'shuffle'
 
-for model in 'gpt2' do #'gpt2-medium' 'gpt2-large' 'gpt2-xl'; do
+for model in 'gpt2'; do #'gpt2-medium' 'gpt2-large' 'gpt2-xl'; do
     for ptb_type in 'char' 'token' 'shuffle'; do
         for last_n_tok in 1 5 10 25 50; do
             python main.py --model $model --ptb-type $ptb_type --num-eval-tokens $last_n_tok
