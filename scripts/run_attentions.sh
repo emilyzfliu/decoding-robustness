13bfa189c143c5342455373ac72cd645235d0a19
emilyzfliu <emily.zf.liu@gmail.com>
2026-08-21 00:39:05 -0700

    no more debug

--- scripts/run_attentions.sh
+++ scripts/run_attentions.sh
@@ -1,15 +1,15 @@
 export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
 
-for ptb_type in 'char' 'token' 'shuffle' 'word' 'synonym' 'typo' 'adv'; do
-    python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 5 --out-root res_attentions_0820_debug --debug
-    python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 30 --out-root res_attentions_0820_debug --debug
-done
+# for ptb_type in 'char' 'token' 'shuffle' 'word' 'synonym' 'typo' 'adv'; do
+#     python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 5 --out-root res_attentions_0820_debug --debug
+#     python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 30 --out-root res_attentions_0820_debug --debug
+# done
 
 
 # after verified
-# for ptb_type in 'char' 'token' 'shuffle' 'word' 'synonym' 'typo' 'adv'; do
-#     python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 5 --out-root res_attentions_0820
-#     python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 30 --out-root res_attentions_0820
-# done
+for ptb_type in 'char' 'token' 'shuffle' 'word' 'synonym' 'typo' 'adv'; do
+    python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 5 --out-root res_attentions_0820
+    python analysis/attentions.py --model 'gpt2' --ptb-type $ptb_type --ptb-pct 30 --out-root res_attentions_0820
+done
 
